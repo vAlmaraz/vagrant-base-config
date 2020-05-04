51c90f32f82af3202e0b2d5c215cc47312976f29
vAlmaraz <vAlmaraz@users.noreply.github.com>
2020-05-04 23:51:57 +0200

    Update setup.sh

--- provision/setup.sh
+++ provision/setup.sh
@@ -2,6 +2,13 @@
 
 echo "Provisioning virtual machine..."
 
+echo "Configuring hostname"
+echo "web" > /etc/hostname
+hostname -F /etc/hostname
+ip=$(ip addr show eth0 | grep -Po 'inet \K[\d.]+')
+echo "$ip   $ip hostname" >> /etc/hosts
+apt-get update && apt-get upgrade -y
+
 # Apache
 echo "Installing Apache"
 apt-get install apache2 -y > /dev/null
