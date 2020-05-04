dd8886d55e03c07e9b297c42aee56ace9f67f2be
vAlmaraz <vAlmaraz@users.noreply.github.com>
2020-05-04 23:44:58 +0200

    Update setup.sh

--- provision/setup.sh
+++ provision/setup.sh
@@ -2,43 +2,35 @@
 
 echo "Provisioning virtual machine..."
 
-# Git
-echo "Installing Git"
-apt-get install git -y > /dev/null
-
-# Nginx
-echo "Installing Nginx"
-apt-get install nginx -y > /dev/null
+# Apache
+echo "Installing Apache"
+apt-get install apache2 -y > /dev/null
 
 # PHP
 echo "Updating PHP repository"
 apt-get install python-software-properties build-essential -y > /dev/null
-add-apt-repository ppa:ondrej/php5 -y > /dev/null
+add-apt-repository ppa:ondrej/php -y > /dev/null
 apt-get update > /dev/null
 
 echo "Installing PHP"
-apt-get install php5-common php5-dev php5-cli php5-fpm -y > /dev/null
+apt-get install php7.2-common php7.2-dev php7.2-cli php7.2-fpm -y > /dev/null
 
 echo "Installing PHP extensions"
-apt-get install curl php5-curl php5-gd php5-mcrypt php5-mysql -y > /dev/null
+apt-get install curl php7.2-curl -y > /dev/null
 
-# MySQL 
-echo "Preparing MySQL"
-apt-get install debconf-utils -y > /dev/null
-debconf-set-selections <<< "mysql-server mysql-server/root_password password 1234"
-debconf-set-selections <<< "mysql-server mysql-server/root_password_again password 1234"
+# PostgreSQL
+echo "Installing PostgreSQL"
+apt-get install postgresql -y > /dev/null
 
-echo "Installing MySQL"
-apt-get install mysql-server -y > /dev/null
+# Apache Configuration
+echo "Configuring Apache"
+cp /var/www/provision/config/web.conf /etc/apache2/sites-available/web.conf > /dev/null
+a2ensite web.conf
 
-# Nginx Configuration
-echo "Configuring Nginx"
-cp /var/www/provision/config/nginx_vhost /etc/nginx/sites-available/nginx_vhost > /dev/null
-ln -s /etc/nginx/sites-available/nginx_vhost /etc/nginx/sites-enabled/
+a2dissite 000-default.conf
+rm /etc/apache2/sites-available/000-default.conf
 
-rm -rf /etc/nginx/sites-available/default
-
-# Restart Nginx for the config to take effect
-service nginx restart > /dev/null
+# Restart Apache for the config to take effect
+service apache2 restart > /dev/null
 
 echo "Finished provisioning."
